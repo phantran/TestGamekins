b798cc87045b5eedafb9a164e90acb2e361bab88
TranPhan <phantran197@gmail.com>
2021-02-20 09:09:38 +0100

    updated

---
+++ src/test/java/org/example/SimpleExampleTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleExampleTest {
+
+    @Test
+    public void test_hihi() {
+        SimpleExample a = new SimpleExample(1);
+        a.doStuff(1, 2.0);
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test_hihi1() {
+        Hihi a = new Hihi(10);
+        a.addRiders(3);
+        a.getTopFloor();
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test_hihi2() {
+        Hihi a = new Hihi(10);
+        a.call(5);
+    }
+}
