7be2b507ee5c30d1043c39683510421e7ab4df41
phantran <phantran197@gmail.com>
2021-03-22 15:33:50 +0100

    asd

--- src/test/java/org/example/FeatureTest.java
+++ src/test/java/org/example/FeatureTest.java
@@ -17,7 +17,7 @@ public class FeatureTest {
     @Test
     public void test_hihi1() {
         Hihi a = new Hihi(326);
-        a.addRiders(1200);
+        a.addRiders(12);
         a.getTopFloor();
         assertEquals(1, 1);
     }
