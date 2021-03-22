3ee5dd37e44b7e56b160ae44f15206ec0659e160
phantran <phantran197@gmail.com>
2021-03-22 15:42:09 +0100

    asd

--- src/test/java/org/example/HeheTest.java
+++ src/test/java/org/example/HeheTest.java
@@ -17,7 +17,7 @@ public class HeheTest {
 
     @Test
     public void test_hihi1() {
-        Hihi a = new Hihi(23);
+        Hihi a = new Hihi(22);
         a.addRiders(5);
         a.getTopFloor();
         assertEquals(1, 1);
