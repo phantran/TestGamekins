bd8381d3d35b7102c43767627f84b976aa2fac50
phantran <phantran197@gmail.com>
2021-03-25 16:02:05 +0100

    asd

--- src/test/java/org/example/HeheTest.java
+++ src/test/java/org/example/HeheTest.java
@@ -14,22 +14,22 @@ public class HeheTest {
         assertEquals(1, 1);
         assertEquals(a.getTopFloor(), 25);
     }
-
-    @Test
-    public void test_hihi1() {
-        Hihi a = new Hihi(22);
-        a.addRiders(32);
-        a.getTopFloor();
-        assertEquals(1, 1);
-        assertEquals(a.getTopFloor(), 22);
-
-    }
-
-    @Test
-    public void test_hihi3() {
-        Hihi a = new Hihi(1);
-        a.addRiders(36);
-        a.getTopFloor();
-        assertEquals(1, 1);
-    }
+//
+//    @Test
+//    public void test_hihi1() {
+//        Hihi a = new Hihi(22);
+//        a.addRiders(32);
+//        a.getTopFloor();
+//        assertEquals(1, 1);
+//        assertEquals(a.getTopFloor(), 22);
+//
+//    }
+//
+//    @Test
+//    public void test_hihi3() {
+//        Hihi a = new Hihi(1);
+//        a.addRiders(36);
+//        a.getTopFloor();
+//        assertEquals(1, 1);
+//    }
 }
