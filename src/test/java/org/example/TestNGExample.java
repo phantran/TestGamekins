d2421031d299c5977bdc3dabc305c7d7ed475f50
phantran <phantran197@gmail.com>
2021-03-25 21:31:23 +0100

    asd

--- src/test/java/org/example/TestNGExample.java
+++ src/test/java/org/example/TestNGExample.java
@@ -16,7 +16,7 @@ public class TestNGExample {
     @Test
     public void LoginTest()
     {
-        Feature a = new Feature(52, 1);
+        Feature a = new Feature(52, 12);
         a.doNothing();
         a.foo(3);
         Assert.assertEquals(1, 1);
